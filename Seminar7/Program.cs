fadc91dd300bac4eb258b71297efe1bb83ca55fd
Alena Timashova <alyona8808@mail.ru>
2022-11-17 21:17:49 +0300

    Seminar 7

---
+++ Seminar7/Program.cs
@@ -0,0 +1,69 @@
+﻿//Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+
+int[,] CreateRandom2dArray(int rows, int columns, int MinValue, int MaxValue)
+{
+    int[,] array = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            array[i, j] = new Random().Next(MinValue, MaxValue + 1);
+
+    return array;
+}
+
+void Show2dArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
+
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Input number of rows: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number of column: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input min number: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input max number: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = CreateRandom2dArray(m, n, min, max);
+Show2dArray(myArray);
+
+//Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aij = i+j. Выведите полученный массив на экран.
+/*
+int [,] CreateArray(int rows, int columns)
+{
+    int [,] array = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            array[i,j] = i+j;
+
+    return array;
+}
+*/
+//Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+
+void SearchAndReplace(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] *= array[i, j];
+}
+
+//Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+
+int SumDiagonale(int[,] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.GetLength(0) && i < array.GetLength(1); i++)
+        sum += array[i, i];
+    return sum;
+
+}
